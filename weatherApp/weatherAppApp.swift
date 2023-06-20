cf9286f7d0abba11ae7b00227f252202362a5fc7
Anmol Gulati <61374198+AnmolGulati6@users.noreply.github.com>
2023-06-20 12:08:28 -0700

    Add files via upload

---
+++ weatherApp/weatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherAppApp.swift
+//  weatherApp
+//
+//  Created by Anmol Gulati on 6/20/23.
+//
+
+import SwiftUI
+
+@main
+struct weatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
